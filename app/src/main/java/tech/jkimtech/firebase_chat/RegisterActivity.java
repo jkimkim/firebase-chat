5dfe21d03404effb618c619fbb21d6a0317ff688
jkimkim <kimjohnklm21@gmail.com>
2022-02-18 01:11:10 +0300

    update

--- app/src/main/java/tech/jkimtech/firebase_chat/RegisterActivity.java
+++ app/src/main/java/tech/jkimtech/firebase_chat/RegisterActivity.java
@@ -1,13 +1,25 @@
 package tech.jkimtech.firebase_chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,6 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
     Button mRegisterBtn;
 
     ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +41,53 @@ public class RegisterActivity extends AppCompatActivity {
         mEmailAd = findViewById(R.id.emailAd);
         mPasswordIn = findViewById(R.id.passIn);
         mRegisterBtn = findViewById(R.id.registerBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Registering user...");
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = mEmailAd.getText().toString().trim();
+                String password = mPasswordIn.getText().toString().trim();
 
+                if (! Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    mEmailAd.setError("invalid email");
+                    mEmailAd.setFocusable(true);
+                }else if (password.length()<6){
+                    mPasswordIn.setError("password too short");
+                    mPasswordIn.setFocusable(true);
+                }else {
+                    registerUser(email, password);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT);
             }
         });
     }
