4d3aef6be86122a1d210d11c44f59527489ff267
jkimkim <kimjohnklm21@gmail.com>
2022-02-18 00:17:11 +0300

    update

--- app/src/main/java/tech/jkimtech/firebase_chat/RegisterActivity.java
+++ app/src/main/java/tech/jkimtech/firebase_chat/RegisterActivity.java
@@ -3,9 +3,18 @@ package tech.jkimtech.firebase_chat;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText mEmailAd, mPasswordIn;
+    Button mRegisterBtn;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,19 @@ public class RegisterActivity extends AppCompatActivity {
         actionBar.setTitle("Create Account");
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        mEmailAd = findViewById(R.id.emailAd);
+        mPasswordIn = findViewById(R.id.passIn);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering user...");
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
